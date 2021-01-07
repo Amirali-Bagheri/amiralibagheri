e4cc17adf088b4a91d13d48b94207705d01070cc
Amirali Bagheri <bagheriamirali2000@gmail.com>
2021-01-07 17:49:24 +0330

    Add Project to Git

---
+++ resources/views/index.blade.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+    <title>Resume - Amirali Bagheri</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico"/>
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css"/>
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css"/>
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="{{asset('css/styles.css')}}" rel="stylesheet"/>
+</head>
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">
+            <span class="d-block d-lg-none">Amirali Bagheri</span>
+            <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2"
+                                                 src="assets/img/profile.jpg" alt=""/></span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
+                class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#works">Works</a></li>
+            </ul>
+        </div>
+    </nav>
+    <!-- Page Content-->
+    <div class="container-fluid p-0">
+        <!-- About-->
+        <section class="resume-section" id="about">
+            <div class="resume-section-content">
+                <h1 class="mb-0">
+                    Amirali
+                    <span class="text-primary">Bagheri</span>
+                </h1>
+                <div class="subheading mb-5">
+                    Web Developer and Application Programmer
+
+                </div>
+                <p class="lead mb-5">I have experience in Website Development and Application Programming.
+                    Im Study Computer Software Engineering at WTIUA.
+                </p>
+                <div class="social-icons">
+                    <a class="social-icon" href="https://t.me/xploiter"><i class="fab fa-telegram"></i></a>
+                    <a class="social-icon" href="https://github.com/Amirali-Bagheri"><i class="fab fa-github"></i></a>
+                    <a class="social-icon" href="https://twitter.com/Amirali2079"><i class="fab fa-twitter"></i></a>
+                    <a class="social-icon" href="https://api.whatsapp.com/send?phone=989129286632"><i class="fab fa-whatsapp"></i></a>
+                    <a class="social-icon" href="https://instagram.com/amirali.b2000"><i class="fab fa-instagram"></i></a>
+                    <a class="social-icon" href="mailto:contact@amiralibagheri.ir"><i class="fas fa-envelope"></i></a>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0"/>
+        <!-- Experience-->
+        <section class="resume-section" id="experience">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Experience</h2>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Advance Back-End Developer</h3>
+                        <p>
+                            I have experience of different Website Development with Laravel Framework and Wordpress.
+                        </p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">4 Years - Advanced</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Middle Front-End Designer</h3>
+                        <p>
+                            I have also worked with some Front-End technologies such as Vuejs , Laravel Livewire , Bootstrap ,
+                            Tailwind and etc...
+                        </p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">4 Years - Middle</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">SEO and UX</h3>
+                        <p>
+                            I have Some information about SEO and UX Tips and Tricks.
+                        </p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">2 Years - Junior</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Android Application</h3>
+                        <p>
+                            I have worked with Android Studio Java
+                        </p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">2 Years - Junior</span></div>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0"/>
+        <!-- Skills-->
+        <section class="resume-section" id="skills">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Skills</h2>
+                <div class="subheading mb-3">Programming Languages & Tools</div>
+                <ul class="list-inline dev-icons">
+                    <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-vuejs"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-android"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-node-js"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-laravel"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-php"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-sass"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-git"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-npm"></i></li>
+                </ul>
+                <div class="subheading mb-3">Workflow</div>
+                <ul class="fa-ul mb-0">
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Mobile-First, Responsive Design
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Cross Browser Testing & Debugging
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Cross Functional Teams
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Agile Development & Scrum
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <hr class="m-0"/>
+
+        <section class="resume-section" id="works">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Works</h2>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Vandad Soft</h3>
+                        <a href="https://vandadsoft.ir"
+                           target="_blank" class="subheading mb-3">Preview</a>
+                    </div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Shago</h3>
+                        <a href="https://shago.ir"
+                           target="_blank" class="subheading mb-3">Preview</a>
+                    </div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Novinshoo</h3>
+                        <a href="https://novinshoo.ir"
+                           target="_blank" class="subheading mb-3">Preview</a>
+                    </div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Autocable</h3>
+                        <a href="https://autocable.ir"
+                           target="_blank" class="subheading mb-3">Preview</a>
+                    </div>
+                </div>
+
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Wire And Cable Dictionary</h3>
+                        <a href="https://vandadsoft.ir/blog/article/معرفی-اپلیکیشن-واژه-نامه-سیم-و-کابل"
+                           target="_blank" class="subheading mb-3">Preview and Download</a>
+                    </div>
+                    <img src="https://vandadsoft.ir/uploads/case-wireandcabledictionary.png" width="200" class="img-fluid">
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">SEO Kit</h3>
+                        <a href="https://vandadsoft.ir/blog/article/معرفی-اپلیکیشن-کیت-حرفه-ای-سئو"
+                           target="_blank" class="subheading mb-3">Preview and Download</a>
+                    </div>
+                    <div class="flex-shrink-0">
+                        <img src="https://vandadsoft.ir/uploads/case-seokit.png" width="200" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Third party plugin JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="{{asset('js/scripts.js')}}"></script>
+</body>
+</html>
